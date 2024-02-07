e82938c48d8fe0ba2fe09df263e00d0f4e6a7bd5
Mykola Rudenko <98877409+MykolaRudenko7@users.noreply.github.com>
2024-02-07 11:13:36 +0200

    add workflow

--- src/middleware.js
+++ src/middleware.js
@@ -19,22 +19,32 @@ export default async function middleware(req, res) {
         body: JSON.stringify({
           refreshToken,
         }),
-      }).then((res) => res.json())
+      })
+        .then((res) => res.json())
+        .catch((err) => {
+          throw err
+        })
 
       if (fetchResponse.success) {
-        const responseWithCookies = NextResponse.rewrite(new URL(PROFILE_PAGE_URL, req.url))
-        responseWithCookies.cookies.set('accessToken', fetchResponse.accessToken)
-        responseWithCookies.cookies.set('refreshToken', fetchResponse.refreshToken)
+        const responseWithCookies = NextResponse.rewrite(new URL(PROFILE_PAGE_URL, req.nextUrl), {
+          status: 302,
+        })
+        responseWithCookies.cookies.set('accessToken', fetchResponse.accessToken, {
+          httpOnly: true,
+        })
+        responseWithCookies.cookies.set('refreshToken', fetchResponse.refreshToken, {
+          httpOnly: true,
+        })
 
         return responseWithCookies
       }
     } catch (error) {
-      return NextResponse.rewrite(new URL(LOGIN_PAGE_ENDPOINT, req.url))
+      return NextResponse.rewrite(new URL(LOGIN_PAGE_ENDPOINT, req.nextUrl))
     }
   }
 
   if (!accessToken && !refreshToken) {
-    return NextResponse.rewrite(new URL(LOGIN_PAGE_ENDPOINT, req.url))
+    return NextResponse.rewrite(new URL(LOGIN_PAGE_ENDPOINT, req.nextUrl))
   }
 
   return NextResponse.next()
--- src/services/productFetchingService.js
+++ src/services/productFetchingService.js
@@ -4,22 +4,14 @@ const { LAPTOPS_ENDPOINT, NEW_PRODUCTS_ENDPOINT } = process.env
 
 export default class ProductsService {
   static async fetchLaptops() {
-    try {
-      const response = await axiosInstance.get(LAPTOPS_ENDPOINT)
+    const response = await axiosInstance.get(LAPTOPS_ENDPOINT)
 
-      return response.data
-    } catch (error) {
-      return null
-    }
+    return response.data
   }
 
   static async fetchNewProducts() {
-    try {
-      const response = await axiosInstance.get(NEW_PRODUCTS_ENDPOINT)
+    const response = await axiosInstance.get(NEW_PRODUCTS_ENDPOINT)
 
-      return response.data
-    } catch (error) {
-      return null
-    }
+    return response.data
   }
 }
